914121660768e10086af1c963722658b5ebdd8ba
PiggybankStudios <PrincessDungeonSmasher@gmail.com>
2024-12-14 10:30:51 -0800

    Tried to get Sine Wave example using soundio compiling, didn't quite get it working but the code now lives in sine_wave.zig and some half finished commands exist in build.bat

--- build/build.bat
+++ build/build.bat
@@ -6,5 +6,7 @@ rem zig test ../main.zig
 zig test ../learning1.zig
 echo [Building...]
 rem zig build-exe ../main.zig
-zig build-exe ../learning1.zig
+rem zig build-exe ../learning1.zig
+rem TODO: Still now sure how to get this compiling fully. Need to figure out how to link with libc
+rem zig build-exe ../sine_wave.zig -L"D:/gamedev/lib" -llibc
 echo [Done!]
